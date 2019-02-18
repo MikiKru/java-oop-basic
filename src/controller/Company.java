802c4beee7bb934fc47187cfa443e4d14f919102
MikiKru <michal_kruczkowski@o2.pl>
2019-02-18 12:59:00 +0100

    oop ex1

---
+++ src/controller/Company.java
@@ -0,0 +1,34 @@
+package controller;
+
+import model.Employee;
+
+public class Company {
+    Employee employees [] = new Employee[10];
+    int index = 0;
+
+    public void addEmployee(
+            String name,
+            String lastname,
+            String PESEL,
+            String birth_date,
+            String empl_date,
+            double salary_net){
+        // utworzenie obiektu pracownika
+        Employee e = new Employee(name, lastname, PESEL, birth_date, empl_date, salary_net);
+        // zapisanie pracownika do listy pracowników
+        employees[index] = e;
+        // wyświetlenie danych zapisanego pracownika
+        System.out.println("Dodano pracownika: " + e);
+    }
+
+    public static void main(String[] args) {
+        Company c = new Company();
+        c.addEmployee("M","K", "1233221123", "2000-02-02","2019-01-01", 10000);
+        c.addEmployee("A","A", "5553221123", "2001-02-02","2019-01-01", 14000);
+        c.addEmployee("B","B", "6663221123", "2002-02-02","2019-01-01", 12000);
+
+    }
+
+
+
+}
