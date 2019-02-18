de34c6728a9b8af8de44c38866784aa7e1d8afc3
MikiKru <michal_kruczkowski@o2.pl>
2019-02-18 13:45:19 +0100

    oop ex1-v2

--- src/controller/Company.java
+++ src/controller/Company.java
@@ -17,16 +17,25 @@ public class Company {
         Employee e = new Employee(name, lastname, PESEL, birth_date, empl_date, salary_net);
         // zapisanie pracownika do listy pracowników
         employees[index] = e;
+        // inkrementacja index-u
+        index++;
         // wyświetlenie danych zapisanego pracownika
         System.out.println("Dodano pracownika: " + e);
+    }
+    public void getAllEmployees(){
+        System.out.println("Lista pracowników:");
+        for (int i = 0; i < index; i++){
+            System.out.println(employees[i]);
+        }
     }
 
     public static void main(String[] args) {
         Company c = new Company();
+        // wywołania metod
         c.addEmployee("M","K", "1233221123", "2000-02-02","2019-01-01", 10000);
         c.addEmployee("A","A", "5553221123", "2001-02-02","2019-01-01", 14000);
         c.addEmployee("B","B", "6663221123", "2002-02-02","2019-01-01", 12000);
-
+        c.getAllEmployees();
     }
 
 
