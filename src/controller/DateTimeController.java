e89d0585cdd304d5a8a8018cd62dafbf01595f3b
MikiKru <michal_kruczkowski@o2.pl>
2019-02-19 12:48:24 +0100

    date_time ex1

---
+++ src/controller/DateTimeController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class DateTimeController {
+    public int getAgeFromStringDate(String date){
+        LocalDate birthdate = LocalDate.parse(date);
+        LocalDate today = LocalDate.now();
+        Period age = Period.between(birthdate,today);
+        System.out.println(age);
+        return age.getYears();
+    }
+
+    public static void main(String[] args) {
+        DateTimeController dtc = new DateTimeController();
+        System.out.println("Twój wiek dokładny to: " + dtc.getAgeFromStringDate("2000-10-01") + " lat");
+    }
+}
