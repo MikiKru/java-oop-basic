74f849f06861c7fb963e0f3aeb7889ed3d299d73
MikiKru <michal_kruczkowski@o2.pl>
2019-02-19 11:29:07 +0100

    date_time date comparator v1

--- src/UserMain.java
+++ src/UserMain.java
@@ -6,26 +6,11 @@ import java.time.LocalDateTime;
 public class UserMain {
     public static void main(String[] args) {
         UserController uc = new UserController();
-        uc.addUser(
-                "M",
-                "K",
-                "m",
-                "k",
-                "mk@mk.pl",
-                "123321234",
-                LocalDate.of(2000,01,01),
-                LocalDateTime.now()
-        );
-        uc.addUser(
-                "X",
-                "Y",
-                "x",
-                "y",
-                "xy@xy.pl",
-                "333333222",
-                LocalDate.of(2003,02,18),
-                LocalDateTime.now()
-        );
+        uc.addUser("M", "K", "m", "k", "mk@mk.pl", "123321234", LocalDate.of(2000,01,01), LocalDateTime.now());
+        uc.addUser("X", "Y", "x", "y", "xy@xy.pl", "333333222", LocalDate.of(2003,02,05), LocalDateTime.now());
+        uc.addUser("A", "A", "a", "a", "aa@aa.pl", "333333222", LocalDate.of(1987,06,10), LocalDateTime.now());
+        uc.addUser("B", "B", "b", "b", "bb@bb.pl", "333333222", LocalDate.of(1999,12,22), LocalDateTime.now());
+        uc.addUser("C", "C", "c", "c", "cc@cc.pl", "333333222", LocalDate.of(1967,03,13), LocalDateTime.now());
         uc.getAllUsers();
     }
 }
--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -11,7 +11,8 @@ public class UserController {
 
     public void addUser(String name, String lastname, String login, String password, String email, String phone, LocalDate birth_date, LocalDateTime registration_date_time){
         User u = new User(name, lastname, login, password, email, phone, birth_date, registration_date_time);
-        u.setRegistration_date_time(u.getRegistration_date_time().plusHours(1));
+        // symulacja przesnunięcia czasowego
+        u.setRegistration_date_time(u.getRegistration_date_time().plusHours(-1));
         users[index] = u;
         index++;
     }
@@ -20,6 +21,14 @@ public class UserController {
             System.out.println(users[i]);
         }
     }
-
+    // metoda porównujące daty urodzenia dwóch użytkowników
+    // jeżli piersza data jest < od drugiej -> true
+    // w przeciwnym razie -> false
+    public boolean birthdateComparator(User u1, User u2){
+        return u1.getBirth_date().isBefore(u2.getBirth_date());
+    }
+    public boolean birthdateComparator(int index1, int index2){
+        return users[index1].getBirth_date().isBefore(users[index2].getBirth_date());
+    }
 
 }
