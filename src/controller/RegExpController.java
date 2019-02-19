9f58e64325a245af1867a2682f821f3b249535ad
MikiKru <michal_kruczkowski@o2.pl>
2019-02-19 16:13:55 +0100

    regexp final

--- src/controller/RegExpController.java
+++ src/controller/RegExpController.java
@@ -13,10 +13,15 @@ public class RegExpController {
         String template = "\\d{11}";
         return Pattern.matches(template, pesel);
     }
-    // +[dd][ddd]-ddd-ddd-ddd lub
+    // +[dd][ddd]-ddd-ddd-ddd lub 321-321-123
     public boolean checkPhoneNo(String phone_no){
-        String template = "^[+]{1}\\d{2,3}-\\d{3}-\\d{3}-\\d{3}";
-        return Pattern.matches(template, phone_no);
+        String template1 = "^[+]{1}\\d{2,3}-\\d{3}-\\d{3}-\\d{3}";
+        String template2 = "\\d{3}-\\d{3}-\\d{3}";
+        return Pattern.matches(template1 + "|" + template2, phone_no);
+    }
+    public boolean checkEmailAddress(String email){
+        String template = "^[a-zA-Z0-9]*[._]?[a-zA-Z0-9]+[@]{1}[a-zA-Z0-9]+[.]{1}[a-zA-Z0-9]+$";
+        return Pattern.matches(template, email);
     }
     public static void main(String[] args) {
         RegExpController rec = new RegExpController();
@@ -33,7 +38,13 @@ public class RegExpController {
         System.out.println(rec.checkPesel("k02332123453"));
         System.out.println("phone");
         System.out.println(rec.checkPhoneNo("+43-543-654-234"));
+        System.out.println(rec.checkPhoneNo("+423-543-654-234"));
         System.out.println(rec.checkPhoneNo("+43-5436-54-234"));
+        System.out.println(rec.checkPhoneNo("436-354-234"));
+        System.out.println("email");
+        System.out.println(rec.checkEmailAddress("michal.kruczkowski@o2.pl"));
+        System.out.println(rec.checkEmailAddress("michal.kruczkowskio2.pl"));
+        System.out.println(rec.checkEmailAddress("michal.kruczkowski@o2"));
     }
 
 }
